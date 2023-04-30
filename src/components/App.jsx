daa9cc3505f46194e1f658b3e49b02e7803c1fe1
Ruslan <Giovingo92@gmail.com>
2023-04-30 09:09:22 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Profile } from './profile/Profile';
-import { Statistics } from './StatisticList/StatisticList';
+import { Statistics } from './statisticList/StatisticList';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
