40ae3ae8c0801e4addf15c5bd8bcdf149fa6f495
Ruslan <Giovingo92@gmail.com>
2023-04-30 08:51:33 +0300

    fourth task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { Profile } from './profile/Profile';
-import { Statistics } from './statistics/Statistics';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -30,6 +32,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
